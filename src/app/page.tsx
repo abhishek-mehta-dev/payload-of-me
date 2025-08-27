8f8c6616f3990148919e4b310e7c6e595c7bd414
dkjha1008 <dkjhachd108@gmail.com>
2025-08-27 12:38:42 +0530

    Fix the issue of hydration error and ensure tge build is successfully made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero"
 import About from "@/components/About"
 import Skills from "@/components/Skills"
 import Experience from "@/components/Experience"
-import Projects from "@/components/Project"
+import Projects from "@/components/Projects"
 import Contact from "@/components/Contact"
 
 export default function Home() {
